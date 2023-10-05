23bcad384b75c6d93798a832ef44947dc53f197a
Viacheslav Markov <markvyach83@gmail.com>
2023-10-05 16:27:37 +0300

    added project

---
+++ NewsExplorerSwiftUI/NewsExplorerSwiftUI/NewsExplorerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsExplorerSwiftUIApp.swift
+//  NewsExplorerSwiftUI
+//
+//  Created by Viacheslav Markov on 05.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsExplorerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
